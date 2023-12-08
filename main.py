52b64dd4a5fd98d1bffda16e876a553e4844a5d9
Tian(Maxwell) Yang <maxwell_yang@outlook.com>
2023-12-08 00:43:25 -0600

    Add Lasso and k-svd code

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pathlib import Path
 import shutil
 from tqdm import tqdm
 import dill as pickle
+import numpy as np
 
 losses = {'l1': nn.L1Loss(), 'l2': nn.MSELoss()}
 
@@ -93,7 +94,7 @@ def train(mlp, train_dataloader, val_dataloader, test_dataloader, train_epochs=1
                     y_pred = mlp(x)
                     val_loss += lossfcn(y_pred, y).item()
                 val_loss /= len(val_dataloader)
-                print('Epoch: {} \tValidation Loss: {:.6f}'.format(epoch, val_loss))
+                # print('Epoch: {} \tValidation Loss: {:.6f}'.format(epoch, val_loss))
                 if val_loss < best_val_loss:
                     best_val_loss = val_loss
                     best_model = mlp.state_dict()
@@ -126,19 +127,14 @@ def train(mlp, train_dataloader, val_dataloader, test_dataloader, train_epochs=1
 
     return mlp, train_history, val_history, test_loss
 
-if __name__ == "__main__":
-    input_d = 100
+def main(x, y):
+    _, input_d = x.shape
     hidden_d = [100, 100]
     output_d = 1
     dropout = True
 
     mlp = MLP(input_d, hidden_d, output_d, dropout=dropout)
-    print(mlp)
-
-    # Generate random data
-    x = torch.randn(1000, input_d)
-    weights = torch.randn(input_d, output_d)
-    y = torch.matmul(x, weights)
+    # print(mlp)
 
     # Dataloaders
     train_ratio = 0.7
@@ -172,7 +168,67 @@ if __name__ == "__main__":
     save_path = "./outputs/test"
     continue_train = False
 
-    mlp, train_history, val_history, test_loss = train(mlp, train_dataloader, val_dataloader, test_dataloader,
-                                                       train_epochs=train_epochs, val_interval=val_interval,
-                                                       lr=lr, loss=loss, device=device, test_model=test_model,
-                                                       save_path=save_path, continue_train=continue_train)
+    return train(mlp, train_dataloader, val_dataloader, test_dataloader,
+                                                    train_epochs=train_epochs, val_interval=val_interval,
+                                                    lr=lr, loss=loss, device=device, test_model=test_model,
+                                                    save_path=save_path, continue_train=continue_train)
+
+def ksvd_n_g_p(dataset, k):
+    U, S, Vt = np.linalg.svd(dataset)
+    s = S[:k]
+    new_S = np.diag(s)
+    n, p = dataset.shape
+
+    new_U = U[:,:k]
+    new_Vt = Vt[:k,:]
+
+    print(new_U.shape, new_S.shape, new_Vt.shape)
+    reconst_matrix = new_U @ new_S @ new_Vt
+
+    return reconst_matrix, new_S
+
+if __name__ == "__main__":
+    dataset = np.loadtxt("final_data.csv", delimiter=",", dtype=str)[1:]
+    _, dataset = np.split(dataset, [3], axis=1)
+    labels, features = np.split(dataset.astype("float32"), [1], axis=1)
+    total_num_of_features = features.shape[1]
+
+    # ksvd
+    result = [0] * (total_num_of_features+1)
+    for k in range(1, total_num_of_features+1):
+        reduced_features, _ = ksvd_n_g_p(features, k)
+        x = torch.from_numpy(reduced_features)
+        y = torch.from_numpy(labels)
+        mlp, train_history, val_history, test_loss = main(x, y)
+        result[k] = test_loss
+
+    for k in range(len(result)):
+        print(k, result[k])
+
+    # Lasso
+    mask = [True] * total_num_of_features
+    result = {}
+    while sum(mask) > 0:
+        local_best_new_mask = mask.copy
+        local_best_loss = None
+
+        for i in range(len(mask)):
+            if not mask[i]: continue
+            new_mask = mask.copy()
+            new_mask[i] = False
+            reduced_features = features[:,new_mask]
+            
+            x = torch.from_numpy(reduced_features)
+            y = torch.from_numpy(labels)
+            mlp, train_history, val_history, test_loss = main(x, y)
+
+            if local_best_loss is None or test_loss < local_best_loss:
+                local_best_loss = test_loss
+                local_best_new_mask = new_mask
+        
+        result[tuple(local_best_new_mask)] = local_best_loss
+        print(local_best_new_mask, local_best_loss)
+        mask = local_best_new_mask
+
+    for mask in result:
+        print(mask, result[mask])
